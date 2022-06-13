57f87de39d9ea86802679f440f70f7df53986d32
Emigdio Torres <emigdio821@gmail.com>
2022-06-12 19:27:44 -0500

    Added theme localstorage support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,21 @@ import {
   ColorSchemeProvider,
   ColorScheme,
 } from "@mantine/core"
-import { useState } from "react"
+import { useHotkeys, useLocalStorage } from "@mantine/hooks"
 import AppContainer from "../components/AppContainer"
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>("light")
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "dark",
+    getInitialValueInEffect: true,
+  })
+
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"))
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]])
+
   return (
     <ColorSchemeProvider
       colorScheme={colorScheme}
